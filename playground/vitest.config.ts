bc60e62e7e5debda3c9fe4089ecafdd72cc1136b
J.c <Jak-Ch-ll@mailbox.org>
2025-10-12 20:12:19 +0200

    chore: fix vitest types test setup

--- playground/vitest.config.ts
+++ playground/vitest.config.ts
@@ -1,7 +1,26 @@
-import { defineVitestConfig } from '@nuxt/test-utils/config'
+import { defineVitestProject } from '@nuxt/test-utils/config'
+import { defineConfig } from 'vitest/config'
 
-export default defineVitestConfig({
+// https://nuxt.com/docs/4.x/getting-started/testing#setup
+export default defineConfig({
   test: {
-    environment: 'nuxt',
+    projects: [
+      await defineVitestProject({
+        test: {
+          name: 'nuxt',
+          environment: 'nuxt',
+        },
+      }),
+      {
+        test: {
+          name: 'types',
+          include: ['tests/**/*.spec-d.ts'],
+          typecheck: {
+            enabled: true,
+            only: true,
+          },
+        },
+      },
+    ],
   },
 })
