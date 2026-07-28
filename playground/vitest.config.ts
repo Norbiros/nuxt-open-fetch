3f8d25f10cdaa3b39d044bfad3228d86b61e64fc
onmax <maximogarciamtnez@gmail.com>
2026-07-28 18:11:23 +0700

    test: simplify build project selection

--- playground/vitest.config.ts
+++ playground/vitest.config.ts
@@ -40,7 +40,6 @@ export default defineConfig({
         test: {
           name: 'build',
           include: ['tests/build/**/*.spec.ts'],
-          environment: 'node',
         },
       },
     ],
